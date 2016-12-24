8d9d52e5522d7f2cb4ae9f5adcc4f2af69fa237a
Tunghsiao Liu <t@sparanoid.com>
2016-12-25 02:42:28 +0800

    feat(js): update for better ux

--- app.js
+++ app.js
@@ -1,57 +1,126 @@
-"use strict";
+var setScheme = (method, el) => {
+  'use strict';
 
-var getInputValue = function(el) {
-  return document.getElementById(el).value;
-}
+  // Helpers
+  var getEl = (el) => {
+    return document.getElementById(el);
+  };
 
-var setInputValue = function(el) {
-  return document.getElementById(el).value;
-}
+  var isHex = (colors) => {
+    return re.test(colors[0]) && re.test(colors[1]) && re.test(colors[2]);
+  };
 
-var setScheme = function(method, el) {
-  var regex = /^([0-9a-f]{3}){1,2}$/i;
-  var scheme = [
-    getInputValue('code-color'),
-    getInputValue('link-color'),
-    getInputValue('bg-color')
-  ];
+  var isScheme = (scheme) => {
+    return re.test(scheme.code) && re.test(scheme.link) && re.test(scheme.bg);
+  };
 
-  if (method === 'init') {
-    console.log('hash');
-    if (window.location.hash) {
-      scheme = window.location.hash.replace(/^#/, '').split(',');
+  var updateScheme = (scheme, arr) => {
+    scheme.code = arr[0];
+    scheme.link = arr[1];
+    scheme.bg = arr[2];
+  };
+
+  var updateSchemeButtons = (scheme) => {
+    getEl('code-color-btn').jscolor.fromString(scheme.code);
+    getEl('link-color-btn').jscolor.fromString(scheme.link);
+    getEl('bg-color-btn').jscolor.fromString(scheme.bg);
+  };
+
+  var updateSchemeInputs = (scheme) => {
+    getEl('code-color').value = scheme.code;
+    getEl('link-color').value = scheme.link;
+    getEl('bg-color').value = scheme.bg;
+  };
+
+  var updateLess = (scheme) => {
+    if (isScheme(scheme)) {
+      less.modifyVars({
+        '@code-color': '#' + scheme.code,
+        '@link-color': '#' + scheme.link,
+        '@bg-color': '#' + scheme.bg
+      });
+    } else {
+      console.log('Scheme invalid');
     }
+  };
+
+  var updateUrl = (scheme) => {
+    window.location.hash = `#${scheme.code}-${scheme.link}-${scheme.bg}`;
+  };
+
+  var updateTitle = (scheme) => {
+    document.title = `Randomized - #${scheme.code} #${scheme.link} #${scheme.bg} - Sparanoid`;
+  };
+
+  var update = (scheme, colors, updateButtons = false) => {
+    updateScheme(scheme, colors);
+    updateSchemeInputs(scheme);
+
+    // Button color and background cannot be triggered automatically when input
+    // changes, need force update
+    if (updateButtons) {
+      updateSchemeButtons(scheme);
+    }
+
+    updateLess(scheme);
+    updateUrl(scheme);
+    updateTitle(scheme);
+  };
+
+  // Variables
+  const re = /^([0-9a-f]{3}){1,2}$/i;
+  const defaultColors = ['00cc80', 'a212d1', 'ffffff'];
+  const scheme = {
+    code: getEl('code-color').value,
+    link: getEl('link-color').value,
+    bg: getEl('bg-color').value
+  };
+  var colors = [];
+
+  var setColorsFromHash = () => {
+    if (window.location.hash) {
+      let hash = window.location.hash.replace(/^#/, '').split('-');
+      if (isHex(hash)) {
+        colors = hash;
+      } else {
+        colors = defaultColors;
+      }
+    } else {
+      colors = defaultColors;
+    }
+  };
+
+  // Init on page load
+  if (method === 'init') {
+    setColorsFromHash();
+    update(scheme, colors);
   }
 
+  // Get scheme from predefined colors
   if (method === 'present') {
-    console.log('present');
-    scheme = el.dataset.scheme.split(',');
+    let colors = el.dataset.scheme.split('-');
+    update(scheme, colors, true);
   }
 
+  // Get scheme from color picker, fire immediately
   if (method === 'picker') {
-    // getInputValue('code-color', scheme[0]);
-    // getInputValue('link-color', scheme[1]);
-    // getInputValue('bg-color', scheme[2]);
+    updateLess(scheme);
   }
 
-  // Set colors based on scheme
-  var
-    code = scheme[0],
-    link = scheme[1],
-    bg = scheme[2];
-
-  // Validate and update scheme
-  if (regex.test(code) && regex.test(link) && regex.test(bg) ) {
-    less.modifyVars({
-      '@code-color': '#' + code,
-      '@link-color': '#' + link,
-      '@bg-color': '#' + bg
-    });
-  } else {
-    console.log('Scheme invalid');
+  // Update URL hash and title when scheme changes via color picker
+  if (method === 'post-picker') {
+    updateUrl(scheme);
+    updateTitle(scheme);
   }
 
-  // Set URL hash
-  window.location.hash = '#' + code + ',' + link + ',' + bg;
-}
+  // Update scheme when URL hash changes
+  if (('onhashchange' in window)) {
+    window.onhashchange = () => {
+      setColorsFromHash();
+      update(scheme, colors, true);
+    };
+  }
+};
+
+// Init
 setScheme('init');
